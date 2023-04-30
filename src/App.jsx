911d0eeff6bb08fb8d45c57eef91b2e54f0ca597
Sraboni <srabonidatta9@gmail.com>
2023-04-30 14:18:37 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Resturant from "./component/Resturant";
+
+function App() {
+  
+
+  return (
+    <>
+      <Resturant/>;
+    </>
+  )
+}
+
+export default App
---
+++ src/component/menuApi.jsx
@@ -0,0 +1,95 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/coffee.jpg",
+    name: "coffee",
+    category: "breakfast",
+    price: "70/-",
+    description:
+      "Coffee sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "images/hyderabadi.jpg",
+    name: "hyderabadi biryani",
+    category: "lunch",
+    price: "250/-",
+    description:
+      "Hyderabadi biryani dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 3,
+    image: "images/sandwich.jpg",
+    name: "sandwich",
+    category: "breakfast",
+    price: "80/-",
+    description:
+      "Sandwich consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "images/chola.jpg",
+    name: "chotpoti",
+    category: "evening",
+    price: "50/-",
+    description:
+      "Chotpoti amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "350/-",
+    description:
+      "Pizza consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "images/chickenbiryani.jpg",
+    name: "Chicken Biryani",
+    category: "dinner",
+    price: "320/-",
+    description:
+      "Chicken Biryani sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60/-",
+    description:
+      "Sweets consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "images/planrice.jpg",
+    name: "Bengali Platter",
+    category: "lunch",
+    price: "280/-",
+    description:
+      "Bengali Platter adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 9,
+    image: "images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "15/-",
+    description:
+      "Samosa at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "images/mutton.jpg",
+    name: "Mutton Biryani",
+    category: "dinner",
+    price: "320/-",
+    description:
+      "Mutton Biryani consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Menu;
